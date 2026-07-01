29ed8362641692fffc24332d6a37a9cf18c2ac2e
matwood18 <matwood18@gmail.com>
2026-07-01 19:56:28 +0000

    Add kernel registry exports

---
+++ lib/kernel/registry/index.ts
@@ -0,0 +1,5 @@
+export type { ForgeCommand } from "./commands";
+export { commands } from "./commands";
+
+export type { NavigationItem, NavigationItemId } from "./navigation";
+export { navigationItems } from "./navigation";
